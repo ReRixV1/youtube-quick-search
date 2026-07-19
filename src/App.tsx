d61bd94c677470d4edff930d5900b7b6b1cc00b4
rerixv1 <alexandergustle@gmail.com>
2026-07-19 22:50:02 +0200

    Initial commit
    Kinda finished it

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [showPopup, setShowPopup] = useState<boolean>(false);
+  const [input, setInput] = useState<string>("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      // @ts-ignore
+      inputRef.current.focus()
+    }
+
+    function handleKeyDown(e: KeyboardEvent) {
+      if (e.code == "KeyK" && e.metaKey) {
+        setShowPopup(!showPopup);
+      }
+      if (e.code == "Escape") {
+        setShowPopup(false);
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown)
+    return () => window.removeEventListener("keydown", handleKeyDown)
+  }, [showPopup])
+
+  if (!showPopup) return null;
+
+  return (
+    <div className="search-popup">
+      <div className="search-header">
+        <span className="search-text">Search YouTube</span>
+        <span className="search-shortcut">Esc</span>
+      </div>
+
+      <input
+        ref={inputRef}
+        type="text"
+        className="input-search"
+        placeholder="Search videos, channels, playlists..."
+        autoComplete="off"
+        spellCheck={false}
+        value={input}
+        onChange={(e) => {
+          setInput(e.currentTarget.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.code != "Enter") return;
+          setShowPopup(false);
+
+          const query = input.trim().replace(" ", "+")
+          window.location.href = `https://www.youtube.com/results?search_query=${query}`
+        }}
+      />
+    </div>
+  )
+}
+
+export default App;
